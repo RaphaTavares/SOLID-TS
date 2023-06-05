38e85aadf5cb71c7b7f8df6fd35d922ea2260954
Raphael Tavares <raphael_tavares2000@hotmail.com>
2023-06-04 23:49:27 -0300

    test: check in unit tests

--- src/services/check-in.spec.ts
+++ src/services/check-in.spec.ts
@@ -1,6 +1,6 @@
 import { InMemoryUsersRepository } from "@/repositories/inMemory/in-memory-users-repository";
 import { GetUserProfileUseCase } from "./get-user-profile";
-import { beforeEach, describe, expect, it } from "vitest";
+import { beforeEach, afterEach, describe, expect, it, vi } from "vitest";
 import { hash } from "bcryptjs";
 import { ResourceNotFoundError } from "./errors/ResourceNotFoundError";
 import { InMemoryCheckInsRepository } from "@/repositories/inMemory/in-memory-check-ins-repository";
@@ -13,7 +13,13 @@ describe("Check in use case", () => {
     beforeEach(() => {
         checkInsRepository = new InMemoryCheckInsRepository();
         sut = new CheckInUseCase(checkInsRepository);
+
+        vi.useFakeTimers();
     });
+
+    afterEach(() => {
+        vi.useRealTimers();
+    })
 
     //hífen do check-in é só quando é substantivo. Quando é verbo é sem hifen
     it("should be able to check in", async () => {
@@ -23,5 +29,35 @@ describe("Check in use case", () => {
         });
 
         expect(checkIn.id).toEqual(expect.any(String));
+    });
+
+    it("should not be able to check in twice in the same day", async () => {
+        vi.setSystemTime(new Date(2022, 0, 28, 8, 0, 0));
+
+        await sut.execute({
+            gymId: 'gym-01',
+            userId: 'user-01'
+        });
+
+        await expect(sut.execute({
+            gymId: 'gym-01',
+            userId: 'user-01'
+        })).rejects.toBeInstanceOf(Error);
+    });
+
+    it("should be able to check in in different days", async () => {
+        vi.setSystemTime(new Date(2022, 0, 28, 8, 0, 0));
+
+        await sut.execute({
+            gymId: 'gym-01',
+            userId: 'user-01'
+        });
+
+        vi.setSystemTime(new Date(2022, 0, 29, 8, 0, 0));
+
+        await expect(sut.execute({
+            gymId: 'gym-01',
+            userId: 'user-01'
+        })).resolves.toBeTruthy();
     });
 })
